333c2530668a77eb366d2bc0ed6191545b0b1595
DeadMeon <36109177+DeadMeon@users.noreply.github.com>
2019-01-14 01:41:55 +0100

    Update case.h

--- Labyrinthe/case.h
+++ Labyrinthe/case.h
@@ -2,12 +2,10 @@
 
 typedef struct Case Case;
 struct Case{
-    int type;
-    int up;
-    int right;
-    int down;
-    int left;
-    int check;
+    int type; //forme de la cases
+    int up; // Porte de la case en direction du haut
+    int right; // Porte de la case en direction de la droite
+    int down; // Porte de la case en direction du bas
+    int left; // Porte de la case en direction de la gauche
+    int check; // valeur qui permet de verifier si la case est liee a d'autres si elle sont equivalente
 };
-
-
